c9d96a11ab4b2c0ec14be3a456477a5fbd57d3a5
Maytheu <maytheu98@gmail.com>
2021-09-01 13:18:04 +0100

    graphql query and mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 const { ApolloServer } = require("apollo-server");
-
-// graphql schema
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Link{
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
+const path = require("path");
+const fs = require("fs");
 
 // dummy data for hacker news
 let links = [
@@ -28,6 +16,11 @@ const resolver = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    test: null,
+    link: (parent, args) => { //single list
+      const findPost = links.findIndex((link) => link.id == args.id);
+      return links[findPost]
+    },
   },
 
   //  manages the link schema
@@ -36,10 +29,45 @@ const resolver = {
     url: (parent) => parent.url,
     description: (parent) => parent.description,
   },
+
+  // post
+  Mutation: {
+    post: (parent, arg) => {
+      let idCount = links.length;
+      const link = {
+        id: `link-${idCount++}`,
+        description: arg.description,
+        url: arg.url,
+      };
+      links.push(link);
+      return link;
+    },
+
+    updatePost: (parent, args) => {
+      const post = {
+        id: args.id,
+        description: args.description,
+        url: args.url,
+      };
+
+      const findPost = links.findIndex((link) => link.id == post.id);
+      links[findPost] = post;
+      return post;
+    },
+
+    deletePost: (parent, id) => {
+      const findPost = links.findIndex((link) => link.id == id.id);
+      const deleted = links.splice(findPost, 1);
+      return deleted;
+    },
+  },
 };
 
 // server
-const server = new ApolloServer({ typeDefs, resolvers: resolver });
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"), //import schema
+  resolvers: resolver,
+});
 
 // listen to server
 server.listen().then(({ url }) => console.log(`Server running on ${url}`));
